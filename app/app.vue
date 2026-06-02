439f6f68f91cb674e7d024bc43620a9528b5da4b
legendairy75 <roncrash1@gmail.com>
2026-06-02 16:54:41 -0500

    init

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is a test project for ci/cd pipelines</h1>
+    this test uses: <br>
+    - git actions <br>
+    - Docker <br>
+    - linux <br>
+    - aws <br>
+
+  </div>
+</template>
